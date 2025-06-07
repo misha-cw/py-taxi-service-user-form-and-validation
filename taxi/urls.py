6d490800b18ace276cbf2523029f227cf4599c5c
misha-cw <bigun.mykhaylo@gmail.com>
2025-06-07 17:25:01 +0300

    Taxi service user form and validation

--- taxi/urls.py
+++ taxi/urls.py
@@ -52,17 +52,17 @@ urlpatterns = [
         name="car-assign-driver",
     ),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path("drivers/create", DriverCreateView.as_view(), name="driver-create"),
+    path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
     path(
-        "drivers/<int:pk>/update",
+        "drivers/<int:pk>/update/",
         DriverUpdateView.as_view(),
         name="driver-update"
     ),
     path(
-        "drivers/<int:pk>/delete",
+        "drivers/<int:pk>/delete/",
         DriverDeleteView.as_view(),
         name="driver-delete"
     ),
